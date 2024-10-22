d118611897ee7a817d8ddeaa81f308245b30c878
Iksdiee <nicita_98@mail.ru>
2024-10-22 03:26:36 +0300

    Топорный even

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,13 +1,40 @@
 import random
-
-GAME_DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".'
+from ..cli import welcome_user
+from . import brain_constants
 
 
 def initiate_even():
-    num = random.randint(0, 100)
-    if num % 2 == 0:
-        correct_answer = 'yes'
-    else:
-        correct_answer = "no"
-    return num, correct_answer
+	num = random.randint(0, 100)
+	if num % 2 == 0:
+        	correct_answer = 'yes'
+	else:
+		correct_answer = "no"
+	return num, correct_answer
 
+
+def even_game():
+	counter = 0
+	user_name = welcome_user()
+	print(brain_constants.GAME_DESCRIPTIONS["brain-even"])
+
+	while counter < 3:
+		question, correct_answer = initiate_even()
+		print(question)
+		user_answer = input()
+
+		if user_answer == correct_answer:
+			print("Correct!")
+			counter += 1
+		else:
+			print(f'''"{user_answer}" is wrong answer ;(. Correct answer was "{correct_answer}"\n Let's try again!''')
+			break
+
+	if counter == 3:
+		print(f"Congratulations,{user_name}")
+
+
+def main():
+	even_game()
+
+if __name__ == "__main__":
+	main()

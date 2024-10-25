9038f7676fc7f54c8f4da3123c55aeb73f83cf18
Iksdiee <nicita_98@mail.ru>
2024-10-25 06:44:08 +0300

    fix progression again

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,34 +1,30 @@
 import random
 from ..brain_engine import start_game
 
-
 def generate_progression():
     start_number = random.randint(0, 10)
     length = random.randint(5, 10)
     increment = random.randint(1, 10)
-    progression = [start_number]
-    counter = 0
+    progression = []
 
-    while len(progression) != length:
-        progression.append(progression[counter] + increment)
-        counter += 1
+    for i in range(length):
+       progression.append(start_number + i * increment)
 
     return progression
 
 def game_progression():
     basis = generate_progression()
-    replace_index = random.randint(0, len(basis)-1)
+    replace_index = random.randint(0, len(basis) - 1)
     answer = basis[replace_index]
     question = ''
 
-    for i in basis:
-        if i == answer:
+    for i in range(len(basis)):
+        if i == replace_index:
             question += ' .. '
         else:
-            question += f' {str(i)} '
+            question += f' {basis[i]} '
 
     return question.strip(), str(answer)
-
 
 def start_game_progression():
     start_game(game_progression, 'game-progression')
